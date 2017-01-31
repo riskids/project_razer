956dd0381a8b810524dc02b3061986628d20a150
riskids <bintangmalamhari@gmail.com>
2017-01-31 10:11:32 -0500

    project_razer

---
+++ ori/Ori_files/custom_css.php
@@ -0,0 +1,262 @@
+body {
+     background-color:#ffffff;             
+}
+
+.carousel_slider_holder {
+    background-color:#ffffff;
+}
+
+h1,
+.blog_holder article .post_info .date,
+.blog_holder_v2 article .post_info .date{
+    color: #141c27;                     
+}
+h2,
+h2 a,
+.dropcap{
+    color: #ffbf5a;                     
+}
+
+.blog_holder article .post_text h2 a,
+.blog_holder_v2 article .post_text h2 a{
+    color: #141c27 !important; }
+h3,h3 a{
+    color: #141c27;
+}
+
+h4,
+h4 a,
+.progress_bars_vertical .progress_number{
+    color: #141c27;
+}
+
+.blog_holder_v2 .post_content_link  h4 a,
+.blog_holder .post_content_link  h4 a,
+.blog_holder_v2 .post_content_quote  h4 a,
+.blog_holder .post_content_quote  h4 a,
+.blog_holder_v3 article .post_content_holder .post_text h4 a{
+    color: #141c27;
+}
+
+h5,
+h5 a,
+.price_in_table .value,
+.price_tables .mark,
+.progress_bars .progress_number,
+.progress_bars_vertical .progress_title,
+.percentage,
+.comment_holder .comment .text .name,
+.list.circle ul>li,
+.list.number ul>li,
+.list.number_with_shadow ul>li,
+.services_holder .service,
+.tabs .tabs-nav li a,
+.price_table_inner ul li.table_title{
+    color: #141c27;
+}
+.price_in_table .price,
+.counter_holder span.counter{
+    color: #141c27;             
+}
+
+.services_holder .service a,
+.highlight,
+#wp-calendar caption,
+#wp-calendar td a,
+.widget.widget_rss li a.rsswidget,
+.qode-like,
+.blog_holder article .post_info .date,
+.blog_holder article .post_info .month,
+.blog_holder_v2.blog_single_holder article .info .left span,
+.blog_holder article .post_text .info_bottom .comments,
+.blog_holder_v2 article .info .right .comments,
+.blog_holder_v3 article .post_info .inner .left .comments,
+.blog_holder_v2 article .post_info .date,
+.blog_holder_v2 article .post_info .month,
+.blog_holder_v2 article  .post_content_link:hover .info,
+.blog_holder article  .post_content_link:hover .info,
+.blog_holder_v2 article .post_content_quote:hover .info,
+.blog_holder_v2 article  .post_content_quote:hover .quote_author,
+.blog_holder article .post_content_quote:hover .info,
+.blog_holder article  .post_content_quote:hover .quote_author,
+.blog_holder_v3 article.link:hover .post_content_holder .post_text .info,
+.blog_holder_v3 article.quote:hover .post_content_holder .post_text .info,
+.blog_holder_v3 article.quote:hover  .quote_author,
+.blog_holder_v3 article .post_info .inner .right,
+.blog_single_holder .info_single_bottom .left span,
+.comment_holder .comment .text .name,
+.comment_holder .comment .text .replay,
+.comment_holder .comment .text .comment-reply-link,
+#respond textarea:focus,
+#respond input[type='text']:focus,
+.contact_form input[type='text']:focus,
+.contact_form textarea:focus,
+.comment #respond textarea:focus,
+.comment #respond input[type='text']:focus,
+#respond input[type='submit'],
+.social_share_icon,
+aside #lang_sel a.lang_sel_sel,
+aside #lang_sel_click a.lang_sel_sel,
+aside #lang_sel a.lang_sel_sel:hover,
+aside #lang_sel_click a.lang_sel_sel:hover
+{
+    color: #141c27;
+}
+
+a:hover,
+p a:hover,
+.blog_holder article .post_text .top_info a:hover,
+.blog_holder_v2 article .info .left a:hover,
+.blog_holder_v2.blog_single_holder article .info .left a,
+.blog_holder article  .post_content_link:hover .info a,
+.blog_holder_v2 article  .post_content_quote:hover .info a,
+.blog_holder article  .post_content_quote:hover .info a,
+.blog_holder_v3 article.link:hover .post_content_holder .post_text .info a,
+.blog_holder_v3 article.quote:hover .post_content_holder .post_text .info a,
+.blog_holder_v3 article .post_content_holder .post_text .info a:hover,
+.blog_single_holder .info_single_bottom .right.single_tags a,
+.pagination ul li a,
+.pagination ul li a:hover,
+.footer_top #lang_sel_list ul li a:hover,
+.footer_top #lang_sel_list a.lang_sel_sel:hover,
+.footer_top #lang_sel_list ul li a.lang_sel_sel,
+aside #lang_sel_list ul li a:hover,
+aside #lang_sel_list a.lang_sel_sel:hover,
+aside #lang_sel_list ul li a.lang_sel_sel
+{
+    color: #738298;    
+}
+
+header{
+    background-color: #ffffff;
+}
+
+.separator{
+    background-color: #d8dade;
+}
+
+.title,
+header,
+div.comment_form,
+.comment_holder ul.comment-list,
+.blog_holder article .post_content_holder,
+.carousel_slider_holder{
+    border-color: #d8dade;
+}
+
+
+nav.main_menu ul li:hover a,
+nav.main_menu ul li.active a{
+    color: #fc7044;
+}
+
+.header_right_widget #lang_sel_list a:hover,
+.header_right_widget #lang_sel_list a.lang_sel_other:hover,
+.header_right_widget #lang_sel_list a.lang_sel_sel,
+.header_right_widget #lang_sel ul > li a.lang_sel_sel,
+.header_right_widget #lang_sel_click ul > li a.lang_sel_sel
+{
+    color: #141c27;
+}
+ 
+.footer_top h5 {
+    color: #fc7044;
+}
+
+@media only screen and (min-width: 480px) and (max-width: 768px){
+    header{
+        background-color: #141C27;
+    }
+}
+
+    
+@media only screen and (max-width: 480px){
+    header{
+        background-color: #141C27;
+    }
+}
+
+span.qode-like {
+display:none;
+}
+
+a.qode-like {
+display:none;
+}
+
+.projects_holder .mix .hover h3 {
+color: #fc7044;
+}
+
+.tinker-list li {
+    list-style-image: url('http://tinkerworlds.com/img/bullet.png');
+}
+
+.service1 li {
+    list-style: none outside none;
+    margin: 10px 0px;
+}
+
+.service1 img {
+    width: 50px;
+    margin-right: 50px;
+    padding-bottom: 10px;
+    padding-left: 5px;
+    vertical-align: middle;
+}
+
+.service3 li {
+    list-style: none outside none;
+    margin: 5px 0px;
+}
+
+.service3 img {
+    height: 30px;
+    margin-right: 50px;
+    margin-top: 20px;
+}
+
+table th {
+    background-color: #fff;
+}
+
+table tr:nth-child(2n+1) td {
+    background-color: #fff;
+}
+
+.contact_form h2 {
+    color: #fc7044;
+}
+
+.dropcap {
+    color: #FFF;
+}
+
+.widget.widget_search form input[type="submit"] {
+    background-color: #fc7044;
+}
+
+.contact_form input[type="text"], .contact_form textarea {
+    background-color: #F1F1F1;
+}
+
+.portfolio_prev a,  .portfolio_next a  {
+    color: #fff;
+    font-weight: 600;
+}
+
+.contact_form input[type="text"]:focus, .contact_form textarea:focus {
+    background-color: #ecf5ff;
+}
+
+.blog_holder_v3 article .post_content_holder {
+    background-color: #F5F5F5;
+}
+
+.blog_holder_v3 article .post_info .inner {
+    background: #FFBF5A;
+}
+
+.blog_holder_v3 article .post_info .blog_like {
+    background: #fc7044;
+}
